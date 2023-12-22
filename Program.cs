8471e48eab06e98c5faa9991c350d516dfd61972
Douglas Teixeira Fernandes <douglas.dtf@gmail.com>
2023-12-22 10:00:44 -0300

    Desafio Concluído!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("11960755195", "S23", "12345678900", 256);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iPhone = new Iphone("11960755195", "Iphone 14 Pro", "96385274100", 512);
+iPhone.Ligar();
+iPhone.InstalarAplicativo("Telegram");
